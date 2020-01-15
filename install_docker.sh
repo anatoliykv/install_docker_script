c4a881376553c88687ad396deeda0222bfb8b9d4
Anatoliy <anatoliykv@gmail.com>
2020-01-15 13:31:40 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 sudo apt update
 apt-get upgrade -y --force-yes -qq
-#sudo apt upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose 
 rm get-docker.sh
-#end of instalation Docker
+#echo "End of instalation Docker"
 sudo usermod -aG docker $USER
-#newgrp docker && exit
-echo -e "Docker was installed $(docker -v)\n$(docker-compose --version)"
+newgrp docker && exit
